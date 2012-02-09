c80f73a3cc4d1b9fd20c2f8f7735f5889d444056
Alex Alexandrov <sw@cartman.(none)>
2012-02-09 20:21:58 +0700

    m

--- lib/SQL/Builder.pm
+++ lib/SQL/Builder.pm
@@ -5,6 +5,7 @@ use strict;
 use Attribute::Args;
 use SQL::Builder::Query::Select;
 use SQL::Builder::Query::Insert;
+use SQL::Builder::Query::Delete;
 
 our $VERSION = 0.01;
 
@@ -18,6 +19,11 @@ sub insert :ARGS('scalar', 'hash') {
     SQL::Builder::Query::Insert::new
     ->into($table, keys %values)
     ->values(values %values);
+}
+
+sub delete :ARGS('scalar', 'list?') {
+  my ($table, @where) = @_;
+  return SQL::Builder::Query::Delete::new($table)->where(@where);
 }
 
 1;
---
+++ lib/SQL/Builder/Query/Delete.pm
@@ -0,0 +1,36 @@
+package SQL::Builder::Query::Delete;
+
+use warnings;
+use strict;
+use parent qw/SQL::Builder::Query/;
+use Attribute::Args;
+use SQL::Builder::TableRef::Table;
+use SQL::Builder::Query;
+use SQL::Builder::Condition;
+use SQL::Builder::Order;
+use SQL::Builder::Limit;
+
+sub new :ARGS('scalar') {
+  my ($table) = @_;
+  return bless { 
+	table => SQL::Builder::TableRef::Table::new($table);
+  };
+}
+
+sub where :ARGS(__PACKAGE__, 'list?') {
+  my ($self, @where) = @_;
+  $self->{where} = SQL::Builder::Condition::new(@where);
+  return $self;
+}
+
+sub toSql :ARGS(__PACKAGE__, DBI::db) {
+  my ($self, $dbh) = @_;
+  die sprintf '%s is not a valid SQL::Builder::Condition', $self->{where} unless UNIVERSAL::isa($self->{where}, 'SQL::Builder::Condition');
+  return sprintf 
+    'DELETE FROM %s WHERE %s', 
+    map { $_->toSql($dbh) }
+    $self->{from},
+    $self->{where},
+}
+
+1;
